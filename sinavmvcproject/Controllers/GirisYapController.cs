5f86a9df6286db6882f22b00bb26b72092a0c2cc
Maruf TİLBİ <maruftilbi7@gmail.com>
2024-04-25 18:51:07 +0300

    Proje dosyası ekle.

---
+++ sinavmvcproject/Controllers/GirisYapController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sinavmvcproject.Models.Entity;
+
+namespace sinavmvcproject.Controllers
+{
+    public class GirisYapController : Controller
+    {
+        mvcprojectEntities db = new mvcprojectEntities();
+        // GET: GirisYap
+        public ActionResult Giris()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Giris(admin t)
+        {
+            var bilgiler = db.admin.FirstOrDefault(x => x.kadı == t.kadı && x.şifre == t.şifre);
+            if (bilgiler != null)
+            {
+                return RedirectToAction("Index", "Ogrenci");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ sinavmvcproject/Controllers/OgrenciController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sinavmvcproject.Models.Entity;
+
+namespace sinavmvcproject.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        mvcprojectEntities db = new mvcprojectEntities();
+        // GET: Ogrenci
+        public ActionResult Index()
+        {
+            var ogrenciler = db.ogrenci.ToList();
+            return View(ogrenciler);
+        }
+    }
+}
