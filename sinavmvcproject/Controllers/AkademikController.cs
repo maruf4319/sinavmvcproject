ed90da3acbba7c5d61ee1b63b0442254eb9baa40
Maruf TİLBİ <maruftilbi7@gmail.com>
2024-04-27 20:56:59 +0300

    commit

---
+++ sinavmvcproject/Controllers/AkademikController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sinavmvcproject.Models.Entity;
+
+namespace sinavmvcproject.Controllers
+{
+    public class AkademikController : Controller
+    {
+        mvcprojectEntities db = new mvcprojectEntities();
+        // GET: Akademik
+        public ActionResult Index()
+        {
+            var akd = db.akademikp.ToList();
+            return View(akd);
+        }
+        [HttpGet]
+        public ActionResult NewPersonel()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewPersonel(akademikp p3)
+        {
+            db.akademikp.Add(p3);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGuncelle(int id)
+        {
+            var akd = db.akademikp.Find(id);
+
+            return View("PersonelGuncelle", akd);
+        }
+        public ActionResult Guncelle(akademikp p)
+        {
+            var akd = db.akademikp.Find(p.id);
+            akd.isim = p.isim;
+            akd.soyisim = p.soyisim;
+            akd.kadı = p.kadı;
+            akd.sifre = p.sifre;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var akd = db.akademikp.Find(id);
+            db.akademikp.Remove(akd);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- sinavmvcproject/Controllers/OgrenciController.cs
+++ sinavmvcproject/Controllers/OgrenciController.cs
@@ -16,5 +16,40 @@ namespace sinavmvcproject.Controllers
             var ogrenciler = db.ogrenci.ToList();
             return View(ogrenciler);
         }
+        [HttpGet]
+        public ActionResult YeniOgrenci()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniOgrenci(ogrenci p3)
+        {
+            db.ogrenci.Add(p3);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult OgrenciGuncelle(int id)
+        {
+            var ogrenci = db.ogrenci.Find(id);
+
+            return View("OgrenciGuncelle", ogrenci);
+        }
+        public ActionResult Guncelle(ogrenci p)
+        {
+            var ogr = db.ogrenci.Find(p.ogid);
+            ogr.ogad = p.ogad;
+            ogr.ogsoyad = p.ogsoyad;
+            ogr.ogbolum = p.ogbolum;
+            ogr.ogkredi = p.ogkredi;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var ogr = db.ogrenci.Find(id);
+            db.ogrenci.Remove(ogr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
